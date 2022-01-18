a19bafcd123c89b563455710bbd7a0485fd53735
Jijie LIU <liujijieseason@gmail.com>
2022-01-18 21:28:53 +0100

    refactor: remove the extra code block

--- learn_src/tests/mycalculatordapp.js
+++ learn_src/tests/mycalculatordapp.js
@@ -67,19 +67,17 @@ describe('mycalculatordapp', () => {
   });
 
   it('Divides two numbers', async function() {
-    it('Divides two numbers', async function() {
-      const calculator = _calculator;
-  
-      await program.rpc.divide(new anchor.BN(10), new anchor.BN(3), {
-        accounts: {
-          calculator: calculator.publicKey,
-        },
-      });
-  
-      const account = await program.account.calculator.fetch(calculator.publicKey);
-      assert.ok(account.result.eq(new anchor.BN(3)));
-      assert.ok(account.remainder.eq(new anchor.BN(1)));
-      assert.ok(account.greeting === "Welcome to Solana");
-    });  
+    const calculator = _calculator;
+
+    await program.rpc.divide(new anchor.BN(10), new anchor.BN(3), {
+      accounts: {
+        calculator: calculator.publicKey,
+      },
+    });
+
+    const account = await program.account.calculator.fetch(calculator.publicKey);
+    assert.ok(account.result.eq(new anchor.BN(3)));
+    assert.ok(account.remainder.eq(new anchor.BN(1)));
+    assert.ok(account.greeting === "Welcome to Solana");
   });
 });
